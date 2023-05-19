e9bb90da843c7e83140878a1cf56a76f4707ad59
CamylleCruz <134069103+CamylleCruz@users.noreply.github.com>
2023-05-19 11:32:57 -0300

    Add files via upload

--- server2.js
+++ server2.js
@@ -1,52 +1,94 @@
 const http = require('http');
 const fs = require('fs');
-const path = require('path');
-
+ 
+// Define o caminho para o arquivo paises.json
+const pathToFile = __dirname + '/paises.json';
+ 
 http.createServer((req, res) => {
+ 
   if (req.url === '/paises') {
-    const filePath = path.join(__dirname, 'paises.json');
-    const readStream = fs.createReadStream(filePath);
-
-    res.writeHead(200, {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
-    });
-
-    // Lê o arquivo paises.json e converte para objeto JavaScript
+    // Define o cabeçalho da resposta para JSON
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+ 
+    // Lê o arquivo paises.json
+    fs.readFile(pathToFile, (err, data) => {
       if (err) {
         console.error(err);
-        res.end(JSON.stringify({ error: 'Falha ao ler o arquivo de países.' }));
+        res.statusCode = 500;
+        res.end('Erro interno do servidor');
         return;
       }
-
-      // Converte o conteúdo do arquivo para objeto JSON
-      const countries = JSON.parse(data);
-
-      // Filtra os países que começam com a letra "B"
-      const countriesStartingWithB = [];
-      countries.forEach((country) => {
-        if (country.name.startsWith('B')) {
-          countriesStartingWithB.push(country);
+ 
+      // Analisa o conteúdo JSON
+      const paises = JSON.parse(data);
+ 
+      var paisesPrimeiros20 = [];
+ 
+      paises.forEach((element, index) => { // Adiciona um parâmetro "index" à função forEach
+ 
+        if (index < 20) { // Verifica se o índice é menor que 20
+          paisesPrimeiros20.push(element.nome_pais);
         }
+ 
       });
-
-      // Envia a resposta com os países filtrados em formato JSON
-      res.end(JSON.stringify(countriesStartingWithB));
-
-    }
-      else{
-        const filePath = path.join(__dirname, 'index.html');
-        const readStream = fs.createReadStream(filePath);
-    
-        res.writeHead(200, {
-          'Content-Type': 'text/html',
-          'Access-Control-Allow-Origin': '*'
-        });
-    
-        readStream.pipe(res);
+ 
+      // Gera a resposta HTTP com o conteúdo JSON contendo apenas os primeiros 20 países
+      res.statusCode = 200;
+      res.end(JSON.stringify(paisesPrimeiros20));
+    });
+  }
+ 
+  else if (req.url === '/paisescomb') {
+    // Define o cabeçalho da resposta para JSON
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+ 
+    // Lê o arquivo paises.json
+    fs.readFile(pathToFile, (err, data) => {
+      if (err) {
+        console.error(err);
+        res.statusCode = 500;
+        res.end('Erro interno do servidor');
+        return;
       }
-    }).
-
-  listen(8000);
-
-console.log('Visite-me em: http://localhost:8000');
+ 
+      // Analisa o conteúdo JSON
+      const paises = JSON.parse(data);
+ 
+      var paisescomb = [];
+ 
+      paises.forEach(element => {
+ 
+        var nometemporario = element.nome_pais;
+ 
+        nometemporario = String(nometemporario);
+ 
+        var primeiraletra = nometemporario.substr(0,1);
+ 
+        if(primeiraletra == "B"){
+ 
+          paisescomb.push(element.nome_pais);
+ 
+          console.log(nometemporario);
+ 
+        }
+ 
+      });
+ 
+      // Gera a resposta HTTP com o conteúdo JSON
+      res.statusCode = 200;
+      res.end(JSON.stringify(paisescomb));
+    });
+  } else {
+    // Se a URL não for /paises, envia o arquivo index.html
+    res.writeHead(200, {
+      'Content-Type': 'text/html',
+      'Access-Control-Allow-Origin' : '*'
+    });
+    let readStream = fs.createReadStream(__dirname + '/index.html');
+    readStream.pipe(res);
+  }
+}).listen(5000);
+ 
+console.log('Visite-me em: http://localhost:5000');
